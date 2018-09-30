db8a5c9921573c659c5b81b14a36391ca8cb2ae9
Vladislav Savvateev <savvateevvlad@mail.ru>
2018-09-30 12:40:38 +0800

    Added functional for digit (get digit from number)

--- _obj/Digit.asm
+++ _obj/Digit.asm
@@ -1,5 +1,7 @@
 ; ===========================================================================
 ; Object #02 - Digit (used in HUD)
+; Additional bytes:
+;	$20 - digit position
 ; ===========================================================================
 Obj_Digit:
 		moveq	#0,d0
@@ -16,6 +18,21 @@ Obj_Digit:
 		move.w	#293,2(a0)
 		move.l	#Digit_Map,4(a0)
 		
+		; get sprite by number
+		moveq	#10,d0
+		moveq	#0,d1
+		move.b	$20(a0),d1
+		move.l	$FFFF1002,d2
+		mulu	d1,d0
+		beq.s	@skip
+		divu	d0,d2
+@skip	and.l	#$0000FFFF,d2
+		divu	#10,d2
+		lsr.l	#8,d2		
+		lsr.l	#8,d2		
+		move.b	d2,$10(a0)
+		
+
 @Display:
 		jmp		DisplaySprite
 		
