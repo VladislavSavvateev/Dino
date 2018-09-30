4005ec46040f14f74b5cfdd862242e07a46d4e7f
Vladislav Savvateev <savvateevvlad@mail.ru>
2018-09-30 13:29:47 +0800

    Fixed digits (wrong calculations)

--- _obj/Digit.asm
+++ _obj/Digit.asm
@@ -11,19 +11,22 @@ Obj_Digit:
 ; ---------------------------------------------------------------------------
 @Routines:
 		dc.w	@Main-@Routines
-		dc.w	@Display-@Routines
+		dc.w	@GetNumber-@Routines
 ; ---------------------------------------------------------------------------
 @Main:
 		addq.b	#2,1(a0)
 		move.w	#293,2(a0)
 		move.l	#Digit_Map,4(a0)
-		
+@GetNumber:		
 		; get sprite by number
-		moveq	#10,d0
+		moveq	#1,d0
+		move.l	$FFFF1002,d2
 		moveq	#0,d1
 		move.b	$20(a0),d1
-		move.l	$FFFF1002,d2
-		mulu	d1,d0
+		beq.s	@skip
+		subq.b	#1,d1
+@m		mulu	#10,d0
+		dbf		d1,@m
 		beq.s	@skip
 		divu	d0,d2
 @skip	and.l	#$0000FFFF,d2
@@ -32,8 +35,6 @@ Obj_Digit:
 		lsr.l	#8,d2		
 		move.b	d2,$10(a0)
 		
-
-@Display:
 		jmp		DisplaySprite
 		
 ; ---------------------------------------------------------------------------
