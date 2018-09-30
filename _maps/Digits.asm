c0ab6ced67c6bed4b182b2404fc14b6bbcd33b2c
Vladislav Savvateev <savvateevvlad@mail.ru>
2018-09-30 11:31:34 +0800

    Moved misplaced mapping

---
+++ _maps/Digits.asm
@@ -0,0 +1,34 @@
+; --------------------------------------------------------------------------------
+; Sprite mappings - output from SonMapEd - Sonic 1 format
+; --------------------------------------------------------------------------------
+
+SME_3qY4K:	
+		dc.w SME_3qY4K_16-SME_3qY4K, SME_3qY4K_1C-SME_3qY4K	
+		dc.w SME_3qY4K_22-SME_3qY4K, SME_3qY4K_28-SME_3qY4K	
+		dc.w SME_3qY4K_2E-SME_3qY4K, SME_3qY4K_34-SME_3qY4K	
+		dc.w SME_3qY4K_3A-SME_3qY4K, SME_3qY4K_40-SME_3qY4K	
+		dc.w SME_3qY4K_46-SME_3qY4K, SME_3qY4K_4C-SME_3qY4K	
+		dc.w SME_3qY4K_52-SME_3qY4K	
+SME_3qY4K_16:	dc.b 1	
+		dc.b 0, 0, 0, 0, 0	
+SME_3qY4K_1C:	dc.b 1	
+		dc.b 0, 0, 0, 1, 0	
+SME_3qY4K_22:	dc.b 1	
+		dc.b 0, 0, 0, 2, 0	
+SME_3qY4K_28:	dc.b 1	
+		dc.b 0, 0, 0, 3, 0	
+SME_3qY4K_2E:	dc.b 1	
+		dc.b 0, 0, 0, 4, 0	
+SME_3qY4K_34:	dc.b 1	
+		dc.b 0, 0, 0, 5, 0	
+SME_3qY4K_3A:	dc.b 1	
+		dc.b 0, 0, 0, 6, 0	
+SME_3qY4K_40:	dc.b 1	
+		dc.b 0, 0, 0, 7, 0	
+SME_3qY4K_46:	dc.b 1	
+		dc.b 0, 0, 0, 8, 0	
+SME_3qY4K_4C:	dc.b 1	
+		dc.b 0, 0, 0, 9, 0	
+SME_3qY4K_52:	dc.b 1	
+		dc.b 0, 4, 0, $A, 0	
+		even
