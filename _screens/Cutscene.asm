c1e17c2ef090f04c400d8693f34752f81e776c9a
Vladislav Savvateev <savvateevvlad@mail.ru>
2018-10-03 11:21:54 +0800

    Injected second frame and dots object to cutscene

--- _screens/Cutscene.asm
+++ _screens/Cutscene.asm
@@ -1,5 +1,11 @@
 ; ===========================================================================
 ; Cutscene Screen
+; Modem LEDs:
+;	$FFFFFB1E
+;	$FFFFFB22
+;	$FFFFFB24
+;	$FFFFFB26
+;	$FFFFFB28
 ; ===========================================================================
 CutsceneScreen:
 		lea		($C00004).l,a6      			; load command VDP
@@ -17,13 +23,102 @@ CutsceneScreen:
 		LoadArtUnc	Cutscene_Art, 13088, $0020
 		LoadMapUnc	Cutscene_MapA, 2240, $C000, 1, 320/8, 224/8
 		LoadMapUnc	Cutscene_MapB, 2240, $E000, (1<<13)+1, 320/8, 224/8
-		LoadPal		Cutscene_Pal, $00, 32
+		LoadPal		Cutscene_Pal, $80, 32
+		
+		move.w	#270,$FFFF3000
+		move.b	#0,$FFFF3002
+		move.w	#120,$FFFF3004
+		moveq	#$FFFFFF8C,d0
+		jsr		PlaySample
 		
 CutsceneScreen_Loop:
 		move.b	#2,($FFFFF62A).w	; set VBlank routine
 		jsr		DelayProgram		; run VBlank
 		
+		jsr		ClearSprites
+		jsr		ObjectRun
+		
+		jsr		@Blink
+		jsr		@Action
+		
 		jmp		CutsceneScreen_Loop
+; ---------------------------------------------------------------------------
+@Blink:
+		cmp.b	#4,$FFFF3002
+		bge.w	@rts
+		
+		subq.w	#1,$FFFF3000
+		beq.s	@cont
+		rts
+		
+@cont	move.w	#3,$FFFF3000
+		moveq	#3,d2
+		lea		$FFFFFB22,a0
+@loop	jsr		RandomNumber
+		btst	d2,d0
+		beq.s	@off
+		move.w	#$000A,(a0)+
+		jmp		@dbf
+@off	move.w	#$0000,(a0)+
+@dbf	dbf		d2,@loop
+		rts
+; ---------------------------------------------------------------------------
+@Action:
+		moveq	#0,d0
+		move.b	$FFFF3002,d0
+		move.w	@Actions(pc,d0.w),d0
+		jmp		@Actions(pc,d0.w)
+; ---------------------------------------------------------------------------
+@Actions:
+		dc.w	@FadeIn-@Actions
+		dc.w	@Wait-@Actions
+		dc.w	@FadeIn-@Actions
+		dc.w	@LoadNewArt-@Actions
+		dc.w	@FadeIn-@Actions
+		dc.w	@rts-@Actions
+; ---------------------------------------------------------------------------
+@FadeIn:
+		subq.w	#1,$FFFF3004	; subtract #1 from timer
+		beq.s	@fade			; if it's zero, branch
+		rts
+		
+@fade	move.w	#3,$FFFF3004	; set timer
+		moveq	#31,d0			; set color count
+		jsr		Pal_FadeTo		; run pallet routine
+		tst.b	d3				; changes are more than zero?
+		bne.w	@rts			; if yes, branch
+		
+		jsr		ClearReservedPallete
+		addq.b	#2,$FFFF3002	; next routine
+		move.w	#470,$FFFF3004	; set timer
+		rts
+; ---------------------------------------------------------------------------
+@Wait:
+		subq.w	#1,$FFFF3004	; subtract #1 from timer
+		beq.s	@c				; if it's zero, branch
+		rts
+		
+@c		addq.b	#2,$FFFF3002	; next routine
+		move.w	#3,$FFFF3004
+		rts
+; ---------------------------------------------------------------------------
+@LoadNewArt:
+		LoadArtUnc	Cutscene_Art2, 3712, $0020
+		LoadMapUnc	Cutscene_Map2, 2240, $C000, 1, 320/8, 224/8
+		LoadPal		Cutscene_Pal2, $80, 16
+		
+		LoadArtUnc	Dots_Art, 288, $0EA0
+		LoadPal		Main_Pal, $A0, 16
+		
+		move.b	#3,$FFFF8000
+		
+		addq.b	#2,$FFFF3002	; next routine
+		move.w	#3,$FFFF3004	; set timer
+		
+		rts
+; ---------------------------------------------------------------------------
+@rts:
+		rts
 ; ===========================================================================
 ; Graphic Data
 ; ===========================================================================
@@ -35,3 +130,9 @@ Cutscene_MapB:
 		incbin	"mapunc\1frame_b.bin"
 Cutscene_Pal:
 		incbin	"pallete\1frame.bin"
+Cutscene_Art2:
+		incbin	"artunc\2frame.bin"
+Cutscene_Map2:
+		incbin	"mapunc\2frame.bin"
+Cutscene_Pal2:
+		incbin	"pallete\2frame.bin"
